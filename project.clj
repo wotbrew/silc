068cd11c1ae4f590b8791630149349f6eaaddcb1
Daniel Stone <dan.stone16321@gmail.com>
2014-11-02 02:49:24 +0000

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject silc "0.1.0-SNAPSHOT"
+(defproject silc "0.1.0"
   :description "A simple entity database for clojure"
   :url "http://github.com/danstone/silc"
   :license {:name "Eclipse Public License"
