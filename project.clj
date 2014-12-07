44af09e64cba6ca301fde7b5deb6494a918fc485
danstone <dan.stone16321@gmail.com>
2014-12-07 21:57:53 +0000

    updated readme with new artifact version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject silc "0.1.5"
+(defproject silc "0.1.6-SNAPSHOT"
   :description "A simple entity database for clojure"
   :url "http://github.com/danstone/silc"
   :license {:name "Eclipse Public License"
